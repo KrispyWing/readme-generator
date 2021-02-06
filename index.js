8057e2030646c289741c08bcb6834f841aec6c06
Kris Pennimpede <kpennimpede@hotmail.com>
2021-02-06 16:34:07 -0500

    added name and description questions

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is the title of your Project? (Required)",
+      validate: titleInput => {
+        if (titleInput) {
+          return true;
+        } else {
+          console.log('Please enter your project title!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter a description of your project:'
+    }
+  ];  
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  const readmeData = inquirer.prompt(questions);  
+}
 
 // Function call to initialize app
 init();
+
+
